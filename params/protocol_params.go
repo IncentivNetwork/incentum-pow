9f7edebd2f68a7bf460b0feaf885c1bb6955d233
zpsmcdev <115453065+zpsmcdev@users.noreply.github.com>
2025-09-17 05:29:35 +0300

    Update params for forks

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -188,6 +188,6 @@ var (
 	MinimumDifficulty      = big.NewInt(131072) // The minimum that the difficulty may ever be.
 	DurationLimit          = big.NewInt(13)     // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
 
-	// FeePoolContractAddress Fee pool contract address (temporary test address)
-	FeePoolContractAddress = common.HexToAddress("0x742d35Cc6634C0532925a3b8D4C9db96590c6C89") // TODO: Replace with actual fee pool contract address when deployed
+	// FeePoolContractAddress Fee pool contract address
+	FeePoolContractAddress = common.HexToAddress("0xab4952d4bfE24c9058Ec999c52e131A03284C788")
 )
