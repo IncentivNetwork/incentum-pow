9774399031f45f02ca77a81e38e487b8acd245d3
zpsmcdev <115453065+zpsmcdev@users.noreply.github.com>
2025-09-25 18:33:43 +0300

    Refactor FeePool contract addresses as named constants

--- params/config.go
+++ params/config.go
@@ -35,6 +35,13 @@ var (
 	IncentivMainnetGenesisHash = common.HexToHash("0x08c68c7667787a081a2bef5ddae2f09b968f2ac6a873929cae4591c475376c73")
 )
 
+// FeePool contract addresses for different networks.
+var (
+	DefaultFeePoolAddress           = common.HexToAddress("0xEaCA46260024f4b5a76fccCCa85716B626888361") // Default address for unknown networks (calculated from test mnemonic)
+	IncentivTestnetFeePoolAddress   = common.HexToAddress("0xDCe91614dDf6665d93105D5aCDdaDC02bc64Ed45") // FeeDistributor Proxy address for Incentiv Testnet
+	IncentivMainnetFeePoolAddress   = common.HexToAddress("0x9a86f26E7eC81A9e4d97bE2200fa7b66A82046B2") // FeeDistributor Proxy address for Incentiv Mainnet
+)
+
 // TrustedCheckpoints associates each known checkpoint with the genesis hash of
 // the chain it belongs to.
 var TrustedCheckpoints = map[common.Hash]*TrustedCheckpoint{
@@ -1045,16 +1052,16 @@ func (err *ConfigCompatError) Error() string {
 // GetFeePoolContractAddress returns the appropriate FeePool contract address based on the chain ID
 func (c *ChainConfig) GetFeePoolContractAddress() common.Address {
 	if c.ChainID == nil {
-		return common.HexToAddress("0xEaCA46260024f4b5a76fccCCa85716B626888361") // Return default address if ChainID is nil (calculated from test mnemonic)
+		return DefaultFeePoolAddress
 	}
 
 	switch c.ChainID.Uint64() {
 	case 28802: // Incentiv Testnet
-		return common.HexToAddress("0xDCe91614dDf6665d93105D5aCDdaDC02bc64Ed45") // FeeDistributor Proxy address for Incentiv Testnet
+		return IncentivTestnetFeePoolAddress
 	case 24101: // Incentiv Mainnet
-		return common.HexToAddress("0x9a86f26E7eC81A9e4d97bE2200fa7b66A82046B2") // FeeDistributor Proxy address for Incentiv Mainnet
+		return IncentivMainnetFeePoolAddress
 	default:
-		return common.HexToAddress("0xEaCA46260024f4b5a76fccCCa85716B626888361") // Return default address for unknown networks (calculated from test mnemonic)
+		return DefaultFeePoolAddress
 	}
 }
 
