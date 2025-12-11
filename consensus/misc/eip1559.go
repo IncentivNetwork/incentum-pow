852be94ccf861b707036d7df7eff6ee144e61e27
zpsmcdev <115453065+zpsmcdev@users.noreply.github.com>
2025-12-11 19:58:58 +0200

    Fix CalcBaseFee comment

--- consensus/misc/eip1559.go
+++ consensus/misc/eip1559.go
@@ -82,7 +82,7 @@ func CalcBaseFee(config *params.ChainConfig, parent *types.Header) *big.Int {
 	} else {
 		// Otherwise if the parent block used less gas than its target, the baseFee should decrease.
 		// Compute the decrease amount: parentBaseFee * gasUsedDelta / parentGasTarget / baseFeeChangeDenominator,
-		// subtract it from parentBaseFee, then apply the minimum base fee floor after the calculation below.
+		// subtract it from parentBaseFee, then apply the minimum base fee floor if the fork is activated.
 		num.SetUint64(parentGasTarget - parent.GasUsed)
 		num.Mul(num, parent.BaseFee)
 		num.Div(num, denom.SetUint64(parentGasTarget))
