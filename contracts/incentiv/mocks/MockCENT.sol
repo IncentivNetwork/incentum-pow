e3601f52e7fb7e92a0fc31ac9536717d8185b257
zpsmcdev <115453065+zpsmcdev@users.noreply.github.com>
2026-04-07 07:49:00 +0300

    Add forge fmt config and apply consistent formatting to Solidity files

--- contracts/incentiv/mocks/MockCENT.sol
+++ contracts/incentiv/mocks/MockCENT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.23;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockCENT is ERC20 {
-    constructor() ERC20("Mock CENT", "CENT") {}
+    constructor() ERC20("Mock CENT", "CENT") { }
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
