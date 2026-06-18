699363221acbcf61548e9d1c141fe3ab99d30a95
zpsmcdev <115453065+zpsmcdev@users.noreply.github.com>
2026-06-18 15:28:42 +0300

    Register DynamicMinBaseFeeTime in fork ordering

--- params/config.go
+++ params/config.go
@@ -1034,6 +1034,7 @@ func (c *ChainConfig) CheckConfigForkOrder() error {
 		{name: "mergeNetsplitBlock", block: c.MergeNetsplitBlock, optional: true},
 		{name: "shanghaiTime", timestamp: c.ShanghaiTime},
 		{name: "dpowTime", timestamp: c.DPoWTime, optional: true},
+		{name: "dynamicMinBaseFeeTime", timestamp: c.DynamicMinBaseFeeTime, optional: true},
 		{name: "cancunTime", timestamp: c.CancunTime, optional: true},
 		{name: "pragueTime", timestamp: c.PragueTime, optional: true},
 	} {
