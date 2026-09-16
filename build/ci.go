3a74d1092a6a9da7494d18f413c56854055c15b4
Dmytro Novash <novashdima@gmail.com>
2026-09-16 18:25:12 +0300

    Update golangci-lint & fix graphql test

--- build/ci.go
+++ build/ci.go
@@ -345,7 +345,7 @@ func doLint(cmdline []string) {
 
 // downloadLinter downloads and unpacks golangci-lint.
 func downloadLinter(cachedir string) string {
-	const version = "1.54.2"
+	const version = "1.57.2"
 
 	csdb := build.MustLoadChecksums("build/checksums.txt")
 	arch := runtime.GOARCH
